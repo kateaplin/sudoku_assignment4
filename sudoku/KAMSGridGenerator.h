35d4fac30ec500fb48ddfc0a0c256d9e69d7716d
Kate Aplin <kaplin@hmc.edu>
2014-09-25 15:32:22 -0700

    Created empy GridGenerator class.

---
+++ sudoku/KAMSGridGenerator.h
@@ -0,0 +1,13 @@
+//
+//  KAMSGridGenerator.h
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/25/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KAMSGridGenerator : NSObject
+
+@end
---
+++ sudoku/KAMSGridGenerator.m
@@ -0,0 +1,13 @@
+//
+//  KAMSGridGenerator.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/25/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSGridGenerator.h"
+
+@implementation KAMSGridGenerator
+
+@end
