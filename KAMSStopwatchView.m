7d5ae6d7230336233a834924e65fe406b9a5ea3d
Kate Aplin <kaplin@hmc.edu>
2014-09-28 20:42:58 -0700

    created new files for stopwatch feature

---
+++ KAMSStopwatchView.m
@@ -0,0 +1,21 @@
+//
+//  KAMSStopwatchView.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/28/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSStopwatchView.h"
+
+@implementation KAMSStopwatchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
