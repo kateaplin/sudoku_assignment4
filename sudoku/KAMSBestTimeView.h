6d3e5f88d2f464e1ac877d06e034a3ffb4257c2b
Kate Aplin <kaplin@hmc.edu>
2014-09-28 21:16:16 -0700

    Created Best Time View class.

---
+++ sudoku/KAMSBestTimeView.h
@@ -0,0 +1,13 @@
+//
+//  KAMSBestTimeView.h
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/28/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAMSBestTimeView : UILabel
+
+@end
---
+++ sudoku/KAMSBestTimeView.m
@@ -0,0 +1,21 @@
+//
+//  KAMSBestTimeView.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/28/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSBestTimeView.h"
+
+@implementation KAMSBestTimeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
