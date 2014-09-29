da7b5c5d86c8a7553cfbae4e28f262021b3834fa
Kate Aplin <kaplin@hmc.edu>
2014-09-29 11:28:27 -0700

    Created files for the reset button.

---
+++ sudoku/KAMSResetView.m
@@ -0,0 +1,21 @@
+//
+//  KAMSResetView.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/29/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSResetView.h"
+
+@implementation KAMSResetView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
